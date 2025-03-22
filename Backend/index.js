d5d27b01ce9442e8141140dde9aa2e1605c09378
anuj0704 <anuj.jha.ug22@gmail.com>
2025-03-22 10:52:24 +0530

    Fixed folder creation issue

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const docxToPDF = require("docx-pdf");
 const path = require("path");
 
-
 const app = express();
 const port = 3000;
 
@@ -30,19 +29,19 @@ app.post("/convertFile", upload.single("file"), (req, res, next) => {
             });
         }
         // Defining outout file path
-        let outoutPath = path.join(
+        let outputPath = path.join(
             __dirname,
             "files",
             `${req.file.originalname}.pdf`
         );
-        docxToPDF(req.file.path, outoutPath, (err, result) => {
+        docxToPDF(req.file.path, outputPath, (err, result) => {
             if (err) {
                 console.log(err);
                 return res.status(500).json({
                     message: "Error converting docx to pdf",
                 });
             }
-            res.download(outoutPath, () => {
+            res.download(outputPath, () => {
                 console.log("file downloaded");
             });
         });
